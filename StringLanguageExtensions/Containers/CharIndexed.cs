60e60c77d2bb7a5a7c1eef0ac3568f68f12c958e
Karen Payne <Karen.1.Payne@oregon.gov>
2021-09-21 16:23:21 -0700

    Add project files.

---
+++ StringLanguageExtensions/Containers/CharIndexed.cs
@@ -0,0 +1,13 @@
+﻿namespace StringLanguageExtensions.Containers
+{
+    public class CharIndexed
+    {
+        public char Char { get; set; }
+        public int Index { get; set; }
+
+        public override string ToString()
+        {
+            return $"{{ Char = {Char}, Index = {Index} }}";
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# About 
+
+A collection of C# string language extensions list on the following page.
